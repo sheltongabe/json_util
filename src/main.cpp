5c98050d7bacde9e56550fe1a0a463ebfd3a7099
sheltongabe <sheltongabe0@gmail.com>
2018-07-12 21:52:06 -0400

    Setup CMakeFiles
    Initiailize the project by setting up some CMake files and the main file

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file 		main.cpp
+ * @brief	  serve as the entry point of the program
+ * 
+ * 
+ * 
+ * @author		Gabriel Shelton		sheltongabe
+ * @date 		  07-11-2018
+ * @version		0.1
+ */
+
+#include <iostream>
+
+int main() {
+	return 0;
+}
